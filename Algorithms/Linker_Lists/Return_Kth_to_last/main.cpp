b6fad7ef64bd3b12a6240049196fd96c0ba91310
rustam-ahmadov <rustam.ahmadov114@gmail.com>
2022-09-08 23:17:01 +0400

    Kth_to_Last

--- Algorithms/Linker_Lists/Return_Kth_to_last/main.cpp
+++ Algorithms/Linker_Lists/Return_Kth_to_last/main.cpp
@@ -1,6 +1,6 @@
-#include "..\Linked_List/list.h"
+#include "../Linked_List/list.h"
 
-// from k to the last element
+// O(n)
 void printFromKthToLast(List::node *head, int k)
 {
     List::node *current = head;
@@ -19,19 +19,42 @@ void printFromKthToLast(List::node *head, int k)
         current = current->next;
     }
 }
-
+// O(n)
 int printKthToLast(List::node *head, int k)
 {
     if (!head)
-        return 0;
-    int index = printKthToLast(head->next, k) + 1;
+        return 1;
+    int index = printKthToLast(head->next, k);
     if (index == k)
         std::cout << k << "th to last node is " << head->val;
-    return index;
+    return ++index;
 }
+
+List::node *nthToLast(List::node *head, int k)
+{
+    List::node *p1 = head;
+    List::node *p2 = head;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (head == nullptr)
+            return NULL;
+        p1 = p1->next;
+    }
+
+    while (p1)
+    {
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+
+    return p2;
+}
+
 int main(int argc, char **argv)
 {
     List l;
+
     l.pushBack(0);
     l.pushBack(1);
     l.pushBack(2);
@@ -42,9 +65,12 @@ int main(int argc, char **argv)
     l.pushBack(7);
     l.pushBack(8);
     l.pushBack(9);
-    l.print();
 
-    printKthToLast(l.head, 4);
+    l.print();
+    List::node *kth = nthToLast(l.head, 4);
+    std::cout << kth->val;
+
+    std::cout << '\n';
 
     return 0;
 }
