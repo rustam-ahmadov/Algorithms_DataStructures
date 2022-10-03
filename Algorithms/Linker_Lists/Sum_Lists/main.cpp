131c7f704ee4b737e215f3fcb3200edde8a39cb3
rustam-ahmadov <rustam.ahmadov114@gmail.com>
2022-10-03 13:33:22 +0400

    Sum_Lists

--- Algorithms/Linker_Lists/Sum_Lists/main.cpp
+++ Algorithms/Linker_Lists/Sum_Lists/main.cpp
@@ -1,59 +1,91 @@
 #include "../Linked_List/list.h"
 
-int reverse(int num)
+struct ListNode
 {
-    int reversed = 0;
-    while (num)
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *addSum(ListNode *l1, ListNode *l2)
+{
+    ListNode *tail = nullptr;
+    ListNode *head = nullptr;
+    bool check = false;
+
+    while (l1 || l2)
     {
-        reversed = reversed * 10 + num % 10;
-        num /= 10;
+        int sum = 0, digit1 = 0, digit2 = 0;
+        if (l1)
+            digit1 = l1->val;
+        if (l2)
+            digit2 = l2->val;
+
+        if (check)
+            sum = digit1 + digit2 + 1;
+        else
+            sum = digit1 + digit2;
+
+        if (sum >= 10)
+        {
+            sum = sum % 10;
+            check = true;
+        }
+        else
+            check = false;
+
+        if (head)
+        {
+            tail->next = new ListNode(sum);
+            tail = tail->next;
+        }
+        else
+        {
+            head = new ListNode(sum);
+            tail = head;
+        }
+
+        if (l1)
+            l1 = l1->next;
+        if (l2)
+            l2 = l2->next;
     }
-    return reversed;
+    if (check)
+    {
+        tail->next = new ListNode(1);
+    }
+    return head;
 }
 
-List addList(List::node *head1, List::node *head2)
-{
-    List reversed_list_sum;
-    int head1_num = 0, head2_num = 0;
-    while (head1)
-    {
-        head1_num = head1_num * 10 + head1->val;
-        head1 = head1->next;
-    }
-    while (head2)
-    {
-        head2_num = head2_num * 10 + head2->val;
-        head2 = head2->next;
-    }
-
-    head1_num = reverse(head1_num);
-    head2_num = reverse(head2_num);
-
-    int sum = head1_num + head2_num;
-
-    while(sum)
-    {
-
-        reversed_list_sum.pushBack(sum%10);
-        sum/=10;
-    }
-    return reversed_list_sum;
-
-}
+// 156 = 651
+// 295 = 592
+//     +
+// 3421 1243
 
 int main(int argc, char **argv)
 {
-    List l1;
-    l1.pushBack(7);
-    l1.pushBack(1);
-    l1.pushBack(6);
 
-    List l2;
-    l2.pushBack(5);
-    l2.pushBack(9);
-    l2.pushBack(2);
-    
-    List reversed_list_sum = addList(l1.head,l2.head);
-    reversed_list_sum.print();
+    ListNode *l1 = new ListNode(9);
+    l1->next = new ListNode(9);
+    l1->next->next = new ListNode(9);
+
+    ListNode *l2 = new ListNode(9);
+    l2->next = new ListNode(9);
+    l2->next->next = new ListNode(9);
+    l2->next->next->next = new ListNode(9);
+    l2->next->next->next->next = new ListNode(9);
+
+    ListNode *sum = addSum(l1, l2);
+
+    while (sum)
+    {
+        std::cout << sum->val << ' ';
+        sum = sum->next;
+    }
+
+    std::cout << '\n';
+
     return 0;
 }
