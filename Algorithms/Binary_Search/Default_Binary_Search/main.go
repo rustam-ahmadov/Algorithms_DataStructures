497f5aff7f6f678cc7e262c50160f4ac9c5275aa
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2023-12-04 14:43:10 +0100

    feat: binary search

---
+++ Algorithms/Binary_Search/Default_Binary_Search/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"slices"
+)
+
+func main() {
+	size := 30
+	arr := make([]int, size)
+	fillArrWRandom(arr, size)
+	printArr(arr)
+
+	slices.Sort(arr)
+
+	printArr(arr)
+	isPresent := search(arr, size, 30)
+	fmt.Printf("%t", isPresent)
+}
+
+func fillArrWRandom(arr []int, size int) {
+
+	for i := 0; i < size; i++ {
+		arr[i] = rand.Intn(100) + 1
+	}
+}
+
+func printArr(arr []int) {
+	for _, v := range arr {
+		fmt.Printf("%d ", v)
+	}
+	fmt.Println()
+}
+
+func search(arr []int, size, v int) bool {
+	l, r := 0, size-1
+	
+	for l != r {
+		m := (r + l) / 2
+		if v == arr[m] {
+			return true
+		}
+
+		if v < arr[m] {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	return false
+}
