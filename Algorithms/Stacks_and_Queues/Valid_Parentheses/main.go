18023ef67f0cb519bd30a6400ed61038f5a55277
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2023-12-08 11:45:32 +0100

    feat: valid parentheses in go

---
+++ Algorithms/Stacks_and_Queues/Valid_Parentheses/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+var closingToOpening = map[byte]byte{
+	')': '(',
+	'}': '{',
+	']': '[',
+}
+
+func isValid(s string) bool {
+	str := []byte(s)
+	stack := make([]byte, 0)
+
+	for _, v := range str {
+		opening, isClosing := closingToOpening[v]
+		if !isClosing {
+			stack = append(stack, v)
+			continue
+		}
+
+		if len(stack) == 0{
+			return false
+		}
+
+		if stack[len(stack) - 1] != opening {
+			return false
+		}
+
+		stack = stack[: len(stack) - 1]
+	}
+	return len(stack) == 0
+}
+
+
+func main() {
+
+	// var s1 string = "()"
+	var s2 string = "()[]{}"
+	// var s3 string = "(]"
+	// var s4 string = "({})[]"
+
+	var valid bool = isValid(s2)
+	fmt.Printf("string: %s. isValid: %t\n", s2, valid)
+}
