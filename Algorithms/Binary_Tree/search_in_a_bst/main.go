6359c0d07fbb7efdf7f34af0e9f3fcb8dd1054e1
RustamAhmadov <ahmadov.rustam114@gmail.com>
2023-12-09 18:18:33 +0400

    feat: search in a bst (go)

---
+++ Algorithms/Binary_Tree/search_in_a_bst/main.go
@@ -0,0 +1,26 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	for root != nil {
+		if val == root.Val {
+			return root
+		}
+		if val < root.Val {
+			root = root.Left
+		}else {
+			root = root.Right
+		}
+	}
+	return root
+}
+
+
+func main() {
+
+}
