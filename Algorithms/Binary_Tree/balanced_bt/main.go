76dd9ce5fe6a01eadbc9998e7a32739770091527
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2023-12-12 17:10:01 +0100

    feat: balanced bt

--- Algorithms/Binary_Tree/balanced_bt/main.go
+++ Algorithms/Binary_Tree/balanced_bt/main.go
@@ -8,35 +8,43 @@ type TreeNode struct {
 
 func isBalanced(root *TreeNode) bool {
 	if root == nil {
-        return true
-    }
-	
-	depthLeft := depth(root.Left)
-	depthRight := depth(root.Right)
+		return true
+	}
 
-	if depthLeft == depthRight {
+	depthLeft, isBalancedLeft := depthIsBalanced(root.Left)
+	depthRight, isBalancedRight := depthIsBalanced(root.Right)
+
+	if isBalancedLeft == false || isBalancedRight == false {
+		return false
+	}
+
+	if depthLeft == depthRight || depthLeft == depthRight+1 || depthLeft == depthRight-1 {
 		return true
 	}
-	if depthLeft == depthRight + 1 {
-		return true
-	} 
-	if depthRight == depthLeft + 1 {
-		return true
-	}
+
 	return false
 }
 
-func depth(root *TreeNode) int {
+func depthIsBalanced(root *TreeNode) (depth int, isBalanced bool) {
 	if root == nil {
-		return 0
+		return 0, true
 	}
 	if root.Left == nil && root.Right == nil {
-		return 1
+		return 1, true
 	}
-	depthLeft := depth(root.Left)
-	depthRight := depth(root.Right)
 
-	return max(depthLeft, depthRight) + 1
+	depthLeft, isBalancedLeft := depthIsBalanced(root.Left)
+	depthRight, isBalancedRight := depthIsBalanced(root.Right)
+
+	if isBalancedLeft == false || isBalancedRight == false {
+		return 0, false
+	}
+
+	if depthLeft == depthRight || depthLeft == depthRight+1 || depthLeft == depthRight-1 {
+		return max(depthLeft, depthRight) + 1, true
+	}
+	
+	return 0, false
 }
 
 func main() {
