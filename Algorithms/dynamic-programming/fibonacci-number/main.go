5a48af915a6ffd645c7031ff4ae5d52792b9d186
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2024-02-12 08:56:08 +0400

    perf: version with using memo

--- Algorithms/dynamic-programming/fibonacci-number/main.go
+++ Algorithms/dynamic-programming/fibonacci-number/main.go
@@ -1,12 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+// 0 <= n <= 30
+
+var memo []int = make([]int, 30)
 
 func fib(n int) int {
-    if n == 0 {
-        return 0
+	if n <= 1 {
+		return n
+	}
+    if memo[n]!=0 {
+        return memo[n]
     }
-    if n == 1 || n == 2 {
-        return 1
-    }
-    return fib(n - 1) + fib(n - 2)
+	memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+}
+
+func main(){
+    start:= time.Now()
+    fmt.Println(fib(10))
+    end:= time.Now()
+    fmt.Printf("time %s\n", end.Sub(start))
 }
