3f85805327a2fd0251246d35100386176687a498
rustam-ahmadov <ahmadov.rustam114@gmail.com>
2024-03-07 09:04:02 +0400

    refactor: optimized

--- Algorithms/Strings_Arrays/longest-palindrome/main.go
+++ Algorithms/Strings_Arrays/longest-palindrome/main.go
@@ -3,27 +3,20 @@ func longestPalindrome(s string) int {
 	for i := 0; i < len(s); i++ {
 		m[byte(s[i])]++
 	}
-	odds := []int{}
-	maxOdd := 0
+	one, oddMoreThanOne := false, false
 	res := 0
 	for _, v := range m {
 		if v%2 == 0 {
 			res += v
-		}else if v % 2 != 0 {
-			maxOdd = max(maxOdd, v)
-			odds = append(odds, v)
-		} 
-	}
-	res+=maxOdd
-
-	excludeMaxOdd := false
-	for _, v := range odds {
-		if v == maxOdd && excludeMaxOdd == false {
-			excludeMaxOdd = true
-			continue
-		}else if v > 2 {
+		} else if v%2 != 0 && v != 1 {
+			oddMoreThanOne = true
 			res += v - 1
+		} else {
+			one = true
 		}
 	}
-	return res 
+	if !one && !oddMoreThanOne {
+		return res
+	}
+	return res + 1
 }
