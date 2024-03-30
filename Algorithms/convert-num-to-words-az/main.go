65ec4aaee9eb4725816db661b025ebd5c100c397
Rustam <rustam@MacBook-Pro-Rustam.local>
2024-03-30 17:55:16 +0400

    refactor: version 2

--- Algorithms/convert-num-to-words-az/main.go
+++ Algorithms/convert-num-to-words-az/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 var oneDigitNames = map[int]string{
 	0: "sıfır",
 	1: "bir",
@@ -37,86 +39,81 @@ var threeDigitName = map[int]string{
 	9: "doqquz yüz",
 }
 
-const fourDigitName = "min"
-const sevenDigitName = "milion"
+const fourDigitName = " min"
+const sevenDigitName = " milyon"
+const tenDigitsName = " milyard"
 
 func convert(num int) string {
 	if num == 0 {
 		return oneDigitNames[num]
 	}
-	counter, digitCount := 1, 1
-	isCategoryWordAdded := false
-	return helper(num, isCategoryWordAdded, counter, digitCount)
+	return helper(num, true, 1, 1)
 }
 
-func increase(counter int) int {
-	if counter == 3 {
-		return 1
+func addAdditionalWordInFrontIfNeeded(last, next int, current, additional string) string {
+	if last == 1 && next == 0 {
+		return additional + current
 	}
-	counter++
-	return counter
+	return current
 }
+
 func helper(num int, isCategoryWordAdded bool, counter int, digitCount int) string {
-	if num == 0 {
-		return "" //end
-	}
-
-	last := num % 10
-	if last == 0 { //we just pass it to the next lvl
-		counter = increase(counter)
-		digitCount++
-		return helper(num/10, isCategoryWordAdded, counter, digitCount)
-	}
-
-	var cur string
-	isCategoryHasToChange := false
-	switch counter {
-	case 1:
-		cur = oneDigitNames[last]
-	case 2:
-		cur = twoDigitsNames[last]
-	case 3:
-		cur = threeDigitName[last]
-		isCategoryHasToChange = true
-	}
-
-	if !isCategoryWordAdded {
-		if digitCount > 3 && digitCount < 7 {
-			if last == 1 && num/10 == 0 && counter == 1 {
-				cur = fourDigitName
-			} else {
-				cur += " " + fourDigitName
-			}
-			isCategoryWordAdded = true
-		} else if digitCount > 6 && digitCount < 10 { 
-			cur += " " + sevenDigitName
-			isCategoryWordAdded = true
-		}
-	}
-
-	if isCategoryHasToChange {
+	if counter == 4 {
+		counter = 1
 		isCategoryWordAdded = false
 	}
 
-	counter = increase(counter)
-	digitCount++
-	nextRes := helper(num/10, isCategoryWordAdded, counter, digitCount)
-	return nextRes + cur + " "
+	cur := num % 10
+	leftNum := num / 10
+	if cur == 0 { //we just pass it to the next lvl
+		return helper(leftNum, isCategoryWordAdded, counter+1, digitCount+1)
+	}
+
+	var res string
+	switch counter {
+	case 1:
+		res = oneDigitNames[cur] //1
+	case 2:
+		res = twoDigitsNames[cur] //11
+	case 3:
+		res = threeDigitName[cur] //111
+		res = addAdditionalWordInFrontIfNeeded(cur, leftNum, res, "bir ")
+	}
+
+	if !isCategoryWordAdded && digitCount > 3 {
+		switch digitCount {
+		case 4, 5, 6: //1_000 ... 100_000
+			res += fourDigitName
+		case 7, 8, 9: //1_000_000 ... 100_000_000
+			res += sevenDigitName
+		case 10, 11, 12: //1_000_000_000 ... 100_000_000_000
+			res += tenDigitsName
+		}
+		isCategoryWordAdded = true
+	}
+
+	if leftNum == 0 { //if this is the cur digit
+		return res
+	}
+	return helper(leftNum, isCategoryWordAdded, counter+1, digitCount+1) + " " + res
 }
 
 func main() {
-	println(convert(10))
-	println(convert(1112))
-	println(convert(251))
-	println(convert(45))
-	println(convert(88_921))
-	println(convert(123_456))
-	println(convert(0))
-	println(convert(10))
-	println(convert(100))
-	println(convert(1_000))
-	println(convert(100_001))
-	println(convert(1_000_001)) //todo
-	println(convert(10_000_001))
-	println(convert(11_301_291))
+	fmt.Printf("%s%c\n", convert(1), '*')               //ok
+	fmt.Printf("%s%c\n", convert(10), '*')              //1 2 3
+	fmt.Printf("%s%c\n", convert(100), '*')             //1 2 3
+	fmt.Printf("%s%c\n", convert(1_000), '*')           //1 2 3
+	fmt.Printf("%s%c\n", convert(10_000), '*')          //1 2 3
+	fmt.Printf("%s%c\n", convert(100_000), '*')         //1 2 3
+	fmt.Printf("%s%c\n", convert(1_000_000), '*')       //1 2 3
+	fmt.Printf("%s%c\n", convert(10_000_000), '*')      //1 2 3
+	fmt.Printf("%s%c\n", convert(100_000_000), '*')     //1 2 3
+	fmt.Printf("%s%c\n", convert(1_000_000_000), '*')   //1 2 3
+	fmt.Printf("%s%c\n", convert(10_000_000_000), '*')  //1 2 3
+	fmt.Printf("%s%c\n", convert(113_168_135_431), '*') //1 2 3
+	fmt.Printf("%s%c\n", convert(1123), '*')            //1 2 3
+	fmt.Printf("%s%c\n", convert(353), '*')             //1 2 3
+	fmt.Printf("%s%c\n", convert(349_111_123), '*')     //1 2 3
+	fmt.Printf("%s%c\n", convert(88_999_123), '*')      //1 2 3
+	fmt.Printf("%s%c\n", convert(0), '*')               //1 2 3
 }
